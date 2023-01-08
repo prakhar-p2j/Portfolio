be386ce230a3b034ed598cf255de0bf299d61625
prakhar-p2j <121588365+prakhar-p2j@users.noreply.github.com>
2023-01-07 23:34:31 -0500

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -42,8 +42,8 @@ const PostTemplate = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>{title} | Yashita Namdeo</title>
-        <link rel="canonical" href="https://github.com/yashitanamdeo/pensieve" />
+        <title>{title} | Prakhar Jain</title>
+        <link rel="canonical" href="https://github.com/prakhar-p2j/Portfolio/tree/source/src/pages/pensieve" />
       </Helmet>
 
       <StyledPostContainer>
