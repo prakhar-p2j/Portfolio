9bbf5cfce293a7274d9122f802026c53e85250aa
prakhar-p2j <121588365+prakhar-p2j@users.noreply.github.com>
2023-01-08 03:39:34 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,6 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href=""
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
