6a9e7b780b8009fdad120412a489e243b0a9c415
Prakhar Jain <121588365+prakhar-p2j@users.noreply.github.com>
2023-01-09 04:12:10 -0500

    Update projects.js

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -146,7 +146,7 @@ const Projects = ({ data }) => {
   return (
     <StyledContainer>
       <StyledTitle ref={revealTitle}>Other Noteworthy Projects</StyledTitle>
-      <StyledArchiveLink to="/archive" ref={revealArchiveLink}>
+      <StyledArchiveLink to="" ref={revealArchiveLink}>
         View Complete List of Projects/Codes
       </StyledArchiveLink>
 
